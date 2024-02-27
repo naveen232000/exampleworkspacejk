970e3610248dba0cabdac513f457d04e7867d40a
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-27 17:03:16 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for(int num1 = 1; num1 <= 10; num1++)
+{
+    Console.WriteLine(num1);
+    Thread.Sleep(500);
+}
+for (int num1 = 10; num1 <= 100; num1++)
+{
+    Console.WriteLine(num1);
+    Thread.Sleep(100);
+}
